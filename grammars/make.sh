8f5617805100f7ae4b5666d4aa83d51994efaaa9
Nick Nicholas <opoudjis@gmail.com>
2019-08-03 14:37:47 +1000

    Separate grammar

---
+++ grammars/make.sh
@@ -0,0 +1,18 @@
+if [[ ! -d jing-trang ]]; then
+  git clone https://github.com/relaxng/jing-trang.git
+  cd jing-trang
+  ./ant
+  cd ..
+fi
+
+rm -f relaton-models/grammars/biblio.rng
+git submodule update
+
+cd relaton-models/grammars
+git checkout master && git pull
+sh make.sh
+cd ../..
+cp relaton-models/grammars/biblio.rnc .
+cp relaton-models/grammars/biblio.rng .
+
+java -jar jing-trang/build/trang.jar -I rnc -O rng basicdoc.rnc basicdoc.rng
